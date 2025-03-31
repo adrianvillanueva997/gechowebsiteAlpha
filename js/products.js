e6556520d484346cdbe4dc94ad284bb76a61a9ce
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2025-03-31 23:16:48 +0900

    tours and stuff

---
+++ js/products.js
@@ -0,0 +1,21 @@
+const storeProducts = [
+  {
+    id: 1,
+    name: "C I O K K O",
+    designer: "@yanusthepuss",
+    price: "$10.00",
+    image: "gechoTdesign1.png",
+    alt: "C I O K K O T-shirt",
+    url: "paymentScreen.html"
+  },
+  {
+    id: 2,
+    name: "G E L C O",
+    designer: "@ezpzbundelight",
+    price: "$9.99",
+    image: "gechoTdesign2.png",
+    alt: "G E L C O T-shirt",
+    url: "paymentScreen.html"
+  },
+  // You can easily add more products here
+];
---
+++ js/tours.js
@@ -0,0 +1,128 @@
+const tourDates = [
+  // Current tours
+  {
+    id: 1,
+    date: "APR 15, 2025",
+    venue: "The Civic",
+    city: "Auckland",
+    ticketLink: "https://ticketmaster.co.nz",
+    soldOut: false,
+    featured: true,
+    isPast: false
+  },
+  {
+    id: 2,
+    date: "APR 22, 2025",
+    venue: "San Fran",
+    city: "Wellington",
+    ticketLink: "https://ticketmaster.co.nz",
+    soldOut: false,
+    featured: true,
+    isPast: false
+  },
+  {
+    id: 3,
+    date: "MAY 05, 2025",
+    venue: "Whammy Bar",
+    city: "Auckland",
+    ticketLink: "https://ticketmaster.co.nz",
+    soldOut: false,
+    featured: true,
+    isPast: false
+  },
+  {
+    id: 4,
+    date: "MAY 18, 2025",
+    venue: "Neck of the Woods",
+    city: "Auckland",
+    ticketLink: "https://banishedmusic.com",
+    soldOut: false,
+    isPast: false
+  },
+  {
+    id: 5,
+    date: "JUN 02, 2025",
+    venue: "Wine Cellar",
+    city: "Auckland",
+    ticketLink: "https://undertheradar.co.nz",
+    soldOut: false,
+    isPast: false
+  },
+  {
+    id: 6,
+    date: "JUN 15, 2025",
+    venue: "Meow",
+    city: "Wellington",
+    ticketLink: "https://banishedmusic.com",
+    soldOut: false,
+    isPast: false
+  },
+
+  // Past tours
+  {
+    id: 7,
+    date: "MAR 10, 2025",
+    venue: "Galatos",
+    city: "Auckland",
+    ticketLink: "#",
+    soldOut: true,
+    featured: false,
+    isPast: true,
+    tourName: "Summer Vibes Tour"
+  },
+  {
+    id: 8,
+    date: "FEB 28, 2025",
+    venue: "The Tuning Fork",
+    city: "Auckland",
+    ticketLink: "#",
+    soldOut: true,
+    featured: false,
+    isPast: true,
+    tourName: "Summer Vibes Tour"
+  },
+  {
+    id: 9,
+    date: "FEB 14, 2025",
+    venue: "Valhalla",
+    city: "Wellington",
+    ticketLink: "#",
+    soldOut: true,
+    featured: false,
+    isPast: true,
+    tourName: "Summer Vibes Tour"
+  },
+  {
+    id: 10,
+    date: "JAN 25, 2025",
+    venue: "The Powerstation",
+    city: "Auckland",
+    ticketLink: "#",
+    soldOut: true,
+    featured: false,
+    isPast: true,
+    tourName: "New Year's Explosion"
+  },
+  {
+    id: 11,
+    date: "JAN 10, 2025",
+    venue: "San Fran",
+    city: "Wellington",
+    ticketLink: "#",
+    soldOut: true,
+    featured: false,
+    isPast: true,
+    tourName: "New Year's Explosion"
+  },
+  {
+    id: 12,
+    date: "DEC 15, 2024",
+    venue: "Wine Cellar",
+    city: "Auckland",
+    ticketLink: "#",
+    soldOut: true,
+    featured: false,
+    isPast: true,
+    tourName: "Mystic Winter"
+  }
+];
